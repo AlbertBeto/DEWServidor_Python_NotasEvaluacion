154e15411ce603442dddb211e17290f314e26fcf
AlbertBeto <apbaleyto@gmail.com>
2023-11-17 19:16:23 +0100

    Creando modelos. El de programacion didactica lo llevo a medias

--- avaluaproy/core/models.py
+++ avaluaproy/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Modulo(models.Model):
+    codigo = models.CharField(max_length = 10, verbose_name = "codigo", unique=True)
+    nombre = models.CharField(max_length = 256, verbose_name = "nombre", unique=True)
+
+class ResAprendizaje(models.Model):
+    modulo = models.ForeignKey(Modulo,on_delete=models.PROTECT, unique=True)
+    codigo = models.CharField(max_length = 10, verbose_name="codigo")
+    descripcion = models.Textfield()
+
+class CritEvaluacion(models.Model):
+    resultado_aprendizaje = models.ForeignKey(ResAprendizaje,on_delete=models.PROTECT)
+    codigo = models.CharField(max_length = 4, verbose_name="codigo")
+    descripcion = models.Textfield()
+    minimo = models.BooleanField()
--- avaluaproy/programacion_didactica/models.py
+++ avaluaproy/programacion_didactica/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Unidad(models.Model):
+    codigo = models.CharField(max_length = 4, unique=True)
+    nombre = models.CharField(max_length = 256, unique=True)
+
+class InstEvaluacion(models.Model):
+    codigo = models.CharField(max_length = 4, unique=True)
+    nombre = models.CharField(max_length = 256, unique=True)
+    descripcion = models.TextField()
+
+class PondRA(models.Model):
+    resultado_aprendizaje = models.ForeignKey(ResAprendizaje,on_delete=models.PROTECT)
+    porcentaje = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0.0)
