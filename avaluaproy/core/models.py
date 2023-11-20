74704787579a5f710d656be7852051d8c6a62959
AlbertBeto <apbaleyto@gmail.com>
2023-11-20 17:03:46 +0100

    UD6.3.a y UD6.3.b que son las clases en los modelos Y los str de cada clase

--- avaluaproy/core/models.py
+++ avaluaproy/core/models.py
@@ -1,17 +1,28 @@
 from django.db import models
 
 # Create your models here.
+# UD6.3.a Creo los modelos siguiendo la tabla 1
+# UD6.3.b Creo los str siguiendo tabla 2
 class Modulo(models.Model):
     codigo = models.CharField(max_length = 10, verbose_name = "codigo", unique=True)
     nombre = models.CharField(max_length = 256, verbose_name = "nombre", unique=True)
+
+    def __str__(self):
+        return self.nombre
 
 class ResAprendizaje(models.Model):
     modulo = models.ForeignKey(Modulo,on_delete=models.PROTECT, unique=True)
     codigo = models.CharField(max_length = 10, verbose_name="codigo")
     descripcion = models.Textfield()
 
+    def __str__(self):
+        return {self.codigo}'.'{self.descripcion[:100]}
+
 class CritEvaluacion(models.Model):
     resultado_aprendizaje = models.ForeignKey(ResAprendizaje,on_delete=models.PROTECT)
     codigo = models.CharField(max_length = 4, verbose_name="codigo")
     descripcion = models.Textfield()
     minimo = models.BooleanField()
+
+     def __str__(self):
+        return {self.resultado_aprendizaje}'.'{self.codigo}'.'{self.descripcion[:100]}
--- avaluaproy/programacion_didactica/models.py
+++ avaluaproy/programacion_didactica/models.py
@@ -1,15 +1,42 @@
 from django.db import models
 
 # Create your models here.
+# UD6.3.a Creo los modelos siguiendo la tabla 1
+# UD6.3.b Creo los str siguiendo tabla 2
 class Unidad(models.Model):
     codigo = models.CharField(max_length = 4, unique=True)
     nombre = models.CharField(max_length = 256, unique=True)
+
+    def __str__(self):
+       return {self.codigo}'-'{self.nombre}
 
 class InstEvaluacion(models.Model):
     codigo = models.CharField(max_length = 4, unique=True)
     nombre = models.CharField(max_length = 256, unique=True)
     descripcion = models.TextField()
 
+    def __str__(self):
+        return {self.codigo}'-'{self.nombre}
+
 class PondRA(models.Model):
     resultado_aprendizaje = models.ForeignKey(ResAprendizaje,on_delete=models.PROTECT)
     porcentaje = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0.0)
+
+    def __str__(self):
+        return {str(self.resultado_aprendizaje)}'-'{str(self.porcentaje)}
+
+class PondCriterio(models.Model):
+    criterio_evaluacion = models.ForeignKey(CritEvaluacion,on_delete=models.PROTECT)
+    porcentaje = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0.0)
+    
+    def __str__(self):
+        return {str(self.criterio_evaluacion)}'-'{self.porcentaje}
+
+
+class PondCritUD(models.Model):
+    criterio_evaluacion = models.ForeignKey(CritEvaluacion,on_delete=models.PROTECT)
+    unidad = models.ForeignKey(Unidad,on_delete=models.PROTECT)
+    porcentaje = models.DecimalField(max_digits = 5, decimal_places = 2, default = 0.0)
+
+    def __str__(self):
+        return {str(self.unidad)}':'{str(self.criterio_evaluacion)}'-('+self.porcentaje+')'
