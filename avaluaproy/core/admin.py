897b006ec579f8080c68a74d08f6fefed0a340b4
AlbertBeto <apbaleyto@gmail.com>
2023-11-21 16:18:56 +0100

    6.4.b Creadas las clases en admin de core

--- avaluaproy/core/admin.py
+++ avaluaproy/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+# 6.4.b Implemento las clases según Anexo II
+from .models import Modulo, ResAprendizaje, CritEvaluacion
+
+class ModuloAdmin(admin.ModelAdmin):
+    list_display = list_display_links = ('id','codigo','nombre',)
+    search_fields = ['codigo','nombre',]
+    readonly_fields = ['id',]
+
+class ResAprendizajeAdmin(admin.ModelAdmin):
+    list_display = list_display_links = ('codigo','modulo','descripcion',)
+    list_filter = ('modulo',)
+    search_fields = ['codigo','descripcion',]
+    preserve_fields = [True,]
+
+class CritEvaluacionAdmin(admin.ModelAdmin):
+    list_display = list_display_links = ('resultado_aprendizaje','codigo','descripcion','minimo',)
+    list_filter = ('resultado_aprendizaje','minimo',)
+    search_fields = ['codigo','resultado_aprendizaje','descripcion',]
+    preserve_fields = [True,]
+
+admin.site.register(Modulo, ModuloAdmin)
+admin.site.register(ResAprendizaje, ResAprendizajeAdmin)
+admin.site.register(CritEvaluacion, CritEvaluacionAdmin)
