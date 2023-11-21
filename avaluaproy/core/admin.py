aaedfb4dd2c48e2a8d62e4562efbd6d2b9d2cf2b
AlbertBeto <apbaleyto@gmail.com>
2023-11-21 16:58:23 +0100

    Parado en el admin de programacion_didactica

--- avaluaproy/core/admin.py
+++ avaluaproy/core/admin.py
@@ -13,13 +13,13 @@ class ResAprendizajeAdmin(admin.ModelAdmin):
     list_display = list_display_links = ('codigo','modulo','descripcion',)
     list_filter = ('modulo',)
     search_fields = ['codigo','descripcion',]
-    preserve_fields = [True,]
+    preserve_filters = [True,]
 
 class CritEvaluacionAdmin(admin.ModelAdmin):
     list_display = list_display_links = ('resultado_aprendizaje','codigo','descripcion','minimo',)
     list_filter = ('resultado_aprendizaje','minimo',)
     search_fields = ['codigo','resultado_aprendizaje','descripcion',]
-    preserve_fields = [True,]
+    preserve_filters = [True,]
 
 admin.site.register(Modulo, ModuloAdmin)
 admin.site.register(ResAprendizaje, ResAprendizajeAdmin)
--- avaluaproy/programacion_didactica/admin.py
+++ avaluaproy/programacion_didactica/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+class UnidadAdmin(admin.ModelAdmin):
+    list_display = list_display_links = ('codigo', 'nombre',)
+    search_fields = ['codigo','nombre',]
+    #Con el - delante del campo le digo que lo ordene en descendente
+    ordering = ['-id',]
+
+class InstEvalAdmin(admin.ModelAdmin):
+    list_display = list_display_links = ('codigo','nombre',)
+    search_fields = ['codigo','nombre',]
+
+class PondRAAdmin(admin.ModelAdmin):
+    list_display = list_display_links = ('resultado_aprendizaje','porcentaje',)
+    list_filter = ('resultado_aprendizaje',)
+    search_fields = ['codigo','descripcion',]
+    preserve_filters = [True,]
